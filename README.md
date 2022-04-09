7373fd55ea88bf6001ded13a4581203f71a7313f
Nick Zemskoy <nzemskoy@gmail.com>
2022-04-09 20:00:48 +0300

    Fixed #147

---
+++ README.md
@@ -0,0 +1,11 @@
+# The examples OOP
+
+### Task 1 Use Object simple types
+`Double.valueOf(a)`
+
+```
+Double.valueOf(a);
+Double.valueOf(a);
+Double.valueOf(a);
+```
+
---
+++ src/com/qaforeveryone/opp/ObjectType.java
@@ -0,0 +1,10 @@
+package com.qaforeveryone.opp;
+
+public class ObjectType {
+    public static void main(String[] args) {
+        String a = "1e10";
+
+        System.out.println(Double.valueOf(a));
+
+    }
+}
